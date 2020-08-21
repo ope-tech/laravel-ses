68ed5c0aa13acad0dd21d78a4dc60bbee34beb45
juhasev <juha.vehnia@gmail.com>
2020-08-21 15:35:20 -0500

    Fixed SesMailer constructor call.

--- src/LaravelSesServiceProvider.php
+++ src/LaravelSesServiceProvider.php
@@ -71,7 +71,6 @@ class LaravelSesServiceProvider extends ServiceProvider
             // on the mailer. This allows us to resolve mailer classes via containers
             // for maximum testability on said classes instead of passing Closures.
             $mailer = new SesMailer(
-                'SesMailer',
                 $app['view'],
                 app('mailer')->getSwiftMailer(),
                 $app['events']
